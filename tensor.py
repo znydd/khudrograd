9a84e7db0f66b536ed12830c007294ddb8a0409d
Znyd <mailboxofnabil@gmail.com>
2024-08-06 02:15:22 +0600

    init tensor class with numpy

---
+++ tensor.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+class Tensor:
+
+    def __init__(self, data):
+        self.data = data if isinstance(data, np.ndarray) else np.array(data)
+
+    def __add__(self,other):
+        return Tensor(self.data + other.data)
+    
+    def __mul__(self,other):
+        return Tensor(self.data * other.data)
+    
+    def __repr__(self):
+        return f"tensor({self.data})"
+    
+if __name__ == "__main__":
+    x = Tensor([3])
+    y = Tensor([9])
+    z = x*y 
+    print(z)
+
+
+
+
+
